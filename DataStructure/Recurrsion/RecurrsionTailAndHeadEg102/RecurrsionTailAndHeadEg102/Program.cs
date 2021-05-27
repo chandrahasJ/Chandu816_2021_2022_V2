a7ed5b9a133d171d8da96859a0f228997322235a
Chandu816 <poojari.chandrahas@gmail.com>
2021-05-27 10:05:06 +0530

    #Normal Setup - Recurrsion
    Signed-off-by: Chandu816 <poojari.chandrahas@gmail.com>

---
+++ DataStructure/Recurrsion/RecurrsionTailAndHeadEg102/RecurrsionTailAndHeadEg102/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurrsionTailAndHeadEg102
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Recurrsion r = new Recurrsion();
+            r.calculate(4);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataStructure/Recurrsion/RecurrsionTailAndHeadEg102/RecurrsionTailAndHeadEg102/Recurrsion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurrsionTailAndHeadEg102
+{
+    /// <summary>
+    /// Types of Recurrsion 
+    /// Head , Tail , In-Direct & Tree Recurrsion
+    /// </summary>
+    public class Recurrsion
+    {
+        public void calculate(int n)
+        {
+            if (n > 0)
+            {
+                int k = n * n;
+                Console.WriteLine(k);
+                calculate(n-1);
+            }
+        }
+    }
+}
