88139ade81126b2c48fe7cbb2f716f74b7717f44
Chandu816 <poojari.chandrahas@gmail.com>
2021-06-01 10:27:28 +0530

    Recurrsion.cs for Sum of N Natural Number
    Signed-off-by: Chandu816 <poojari.chandrahas@gmail.com>

---
+++ DataStructure/Recurrsion/RecurrsionEg103/RecurrsionEg103/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurrsionEg103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recurrsion r = new Recurrsion();
+
+
+            Console.WriteLine(r.calculateSumOfNaturalNumberViaFormule(5));
+            Console.WriteLine("-------------------------");
+            Console.WriteLine(r.calculateSumofNatutalNumberViaRecurrsion(5));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataStructure/Recurrsion/RecurrsionEg103/RecurrsionEg103/Recurrsion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurrsionEg103
+{
+    public class Recurrsion
+    {
+        public int calculateSumOfNaturalNumberViaFormule(int n)
+        {
+            int part = n * n + n;
+            return part / 2;
+        }
+
+        public int calculateSumofNatutalNumberViaRecurrsion(int n)
+        {
+            if (n == 1)
+            {
+                return n;
+            }
+
+            return calculateSumofNatutalNumberViaRecurrsion(n - 1) + n;
+        }
+    }
+}
