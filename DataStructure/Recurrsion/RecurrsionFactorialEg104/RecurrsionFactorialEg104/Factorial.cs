7890e408df8f342fae8bff87a7cd26925bf7836a
Chandu816 <poojari.chandrahas@gmail.com>
2021-06-04 09:21:40 +0530

    Factorial Recurrsion Method & Iterative Method
    Signed-off-by: Chandu816 <poojari.chandrahas@gmail.com>

---
+++ DataStructure/Recurrsion/RecurrsionFactorialEg104/RecurrsionFactorialEg104/Factorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurrsionFactorialEg104
+{
+    public class Factorial
+    {
+        public int factorialIterative(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+
+        public int factorialRecurise(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return factorialRecurise(n - 1) * n;
+        }
+    }
+}
---
+++ DataStructure/Recurrsion/RecurrsionFactorialEg104/RecurrsionFactorialEg104/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurrsionFactorialEg104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial f = new Factorial();
+            
+            Console.WriteLine(f.factorialRecurise(5));
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine(f.factorialIterative(5));
+
+            Console.ReadLine();
+        }
+    }
+}
