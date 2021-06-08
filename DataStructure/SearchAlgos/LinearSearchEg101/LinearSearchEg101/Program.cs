24f87200eee9c7460c92aecf8aae768cbe46dd92
Chandu816 <poojari.chandrahas@gmail.com>
2021-06-08 09:16:32 +0530

    LinearSearch
    Signed-off-by: Chandu816 <poojari.chandrahas@gmail.com>

---
+++ DataStructure/SearchAlgos/LinearSearchEg101/LinearSearchEg101/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearchEg101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayData = { 84, 56, 85, 21, 10, 98, 78, 7452, 11, 963 , -100};
+            Search search = new Search();
+            int searchFor = 10;
+            int searchIndex = -1;
+            string searchResult = search.linearSearch(arrayData, arrayData.Length, searchFor, out searchIndex) == -1 ? "Not Found" : "Found";
+            Console.WriteLine($"Searching for {searchFor} ... Result : {searchResult} on index { searchIndex}");
+
+            searchFor = 963;
+            searchResult = search.linearSearch(arrayData, arrayData.Length, searchFor, out searchIndex) == -1 ? "Not Found" : "Found";
+            Console.WriteLine($"Searching for {searchFor} ... Result : {searchResult} on index { searchIndex}");
+
+            searchFor = -100;
+            searchResult = search.linearSearch(arrayData, arrayData.Length, searchFor, out searchIndex) == -1 ? "Not Found" : "Found";
+            Console.WriteLine($"Searching for {searchFor} ... Result : {searchResult} on index { searchIndex}");
+
+            searchFor = 100;
+            searchResult = search.linearSearch(arrayData, arrayData.Length, searchFor, out searchIndex) == -1 ? "Not Found" : "Found";
+            Console.WriteLine($"Searching for {searchFor} ... Result : {searchResult} on index { searchIndex}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataStructure/SearchAlgos/LinearSearchEg101/LinearSearchEg101/Search.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearchEg101
+{
+    public class Search
+    {
+        public int linearSearch(int[] arrayData, int arrayLength, int searchKey, out int searchIndex)
+        {
+            int index = searchIndex  = 0;
+            while (index < arrayLength)
+            {
+                if (arrayData[index] == searchKey)
+                {
+                    searchIndex = index;
+                    return index;
+                }
+
+                index += 1;
+            }
+            searchIndex = -1;
+            return -1;
+        }
+    }
+}
