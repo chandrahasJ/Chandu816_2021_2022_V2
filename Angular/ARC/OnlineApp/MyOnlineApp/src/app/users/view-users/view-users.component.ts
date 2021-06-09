fa34f571c347a714e4d959226e0383afcfe0868f
Chandu816 <poojari.chandrahas@gmail.com>
2021-06-09 09:57:08 +0530

    Observables example - of  operator &  new observable

--- Angular/ARC/OnlineApp/MyOnlineApp/src/app/users/view-users/view-users.component.ts
+++ Angular/ARC/OnlineApp/MyOnlineApp/src/app/users/view-users/view-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { of, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-view-users',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewUsersComponent implements OnInit {
 
+  contacts$ : any;
+  users : any;
+  userStatus : any;
+
   constructor() { }
 
   ngOnInit(): void {
+    //Eg : 1
+    this.users = ['Chandu', 'Hello', 'Compunding','IWillBeBecomeBetterProgrammer'];
+
+    this.contacts$ = of(this.users); //of is used create observables
+    console.log(this.contacts$);
+
+    //Eg :2
+    //Creating observables using new Observables keywords
+    new Observable(observer => {
+
+        setTimeout(() => {
+          observer.next('In Progress');
+        }, 2000);
+
+        setTimeout(() => {
+          observer.next('In Pending Mode');
+        }, 4000);
+
+        setTimeout(() => {
+          observer.next('In Still in Pending Mode');
+        }, 6000);
+
+        setTimeout(() => {
+          observer.next('Completed');
+        }, 8000);
+
+    }).subscribe(data => {
+      this.userStatus = data;
+    },error => {
+      console.log(error)
+    });
+
   }
 
 }
