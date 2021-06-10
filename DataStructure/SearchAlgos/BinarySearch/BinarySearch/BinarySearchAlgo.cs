b679757dad39ee7fb8556434809301fcc12a03d2
Chandu816 <poojari.chandrahas@gmail.com>
2021-06-10 10:45:03 +0530

    Binary Search - Algorithm - Iterative
    Signed-off-by: Chandu816 <poojari.chandrahas@gmail.com>

---
+++ DataStructure/SearchAlgos/BinarySearch/BinarySearch/BinarySearchAlgo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class BinarySearchAlgo
+    {
+        public int BinarySearch(int [] arrayData, int arrayLength, int searchItem)
+        {
+            int left = 0;
+            int right = arrayLength - 1;
+         
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+                if (searchItem == arrayData[middle])
+                {
+                    return middle;
+                }
+                else if (searchItem < arrayData[middle])
+                {
+                    right = middle - 1;
+                }
+                else if (searchItem > arrayData[middle])
+                {
+                    left = middle + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ DataStructure/SearchAlgos/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchAlgo bs = new BinarySearchAlgo();
+            int[] ArrayData = { 15, 26, 95, 100, 106, 500, 589, 700, 896, 999 };
+            foreach (var item in ArrayData)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+            int searchItem = 26;
+            int found = bs.BinarySearch(ArrayData, ArrayData.Length, searchItem);
+            Console.WriteLine($"Result for {searchItem}: " + found);
+
+            searchItem = 106;
+            found = bs.BinarySearch(ArrayData, ArrayData.Length, searchItem);
+            Console.WriteLine($"Result for {searchItem}: " + found);
+
+            searchItem = 700;
+            found = bs.BinarySearch(ArrayData, ArrayData.Length, searchItem);
+            Console.WriteLine($"Result for {searchItem}: " + found);
+
+            searchItem = 1000;
+            found = bs.BinarySearch(ArrayData, ArrayData.Length, searchItem);
+            Console.WriteLine($"Result for {searchItem}: " + found);
+
+            searchItem = 500;
+            found = bs.BinarySearch(ArrayData, ArrayData.Length, searchItem);
+            Console.WriteLine($"Result for {searchItem}: " + found);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
