b0043047921e4b8d15ea46d05ef9d6f3605b0507
Chandu816 <poojari.chandrahas@gmail.com>
2021-06-18 09:52:40 +0530

    Used @Input Directive for adding passing data.

--- Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/property/property-card/property-card.component.ts
+++ Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -6,13 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-card.component.css']
 })
 export class PropertyCardComponent implements OnInit {
-
-  Property : any = {
-    "Id" : 1,
-    "Name" : "Birla House",
-    "Type" : "House",
-    "Price" : 12000
-  };
+  @Input() Property : any;
 
   constructor() { }
 
--- Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/property/property-list/property-list.component.ts
+++ Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -3,13 +3,73 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
+  propertyCollections: Array<any> = [
+    {
+      Id: 1,
+      Name: 'Birla House',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      Id: 2,
+      Name: 'C House',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      Id: 3,
+      Name: 'P House',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      Id: 4,
+      Name: 'A House',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      Id: 5,
+      Name: 'B House',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      Id: 6,
+      Name: 'AB House',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      Id: 7,
+      Name: 'Chandrika House',
+      Type: 'House',
+      Price: 12000000,
+    },
+    {
+      Id: 8,
+      Name: 'Yogesh House',
+      Type: 'Villa',
+      Price: 12000,
+    },
+    {
+      Id: 9,
+      Name: 'Sandeep House',
+      Type: 'Apartment',
+      Price: 12000,
+    },
+    {
+      Id: 10,
+      Name: 'Public House',
+      Type: 'House',
+      Price: 12000,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
