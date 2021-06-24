e3a96be0e88d7d7164c36ff6f11a4c4604961ee8
Chandu816 <poojari.chandrahas@gmail.com>
2021-06-24 09:22:11 +0530

    Add Routing,routerLink,  [routerLinkActiveOptions]

--- Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/app-routing.module.ts
+++ Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', component : PropertyListComponent},
+  { path : 'rent-property', component : PropertyListComponent},
+  { path : 'add-property', component : AddPropertyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/app.module.ts
+++ Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,18 +9,25 @@ import { PropertyCardComponent } from './property/property-card/property-card.co
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+
+const appRoutes : Routes = [
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
-    NavBarComponent
+    NavBarComponent,
+    AddPropertyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [
     HousingService
