067bcbea894fca41974fac4d3bde9ff0800e854a
Chandu816 <poojari.chandrahas@gmail.com>
2021-07-06 22:01:54 +0530

    Added Services/IDataService.cs
    Signed-off-by: Chandu816 <poojari.chandrahas@gmail.com>

---
+++ VS/Normal/SimpleTraderApp/SimpleTraderApp.Domain/Services/IDataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTraderApp.Domain.Services
+{
+    public interface IDataService<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Create(T entity);
+        Task<T> Update(int id, T entity);
+
+        Task<bool> Delete(int id);
+    }
+}
