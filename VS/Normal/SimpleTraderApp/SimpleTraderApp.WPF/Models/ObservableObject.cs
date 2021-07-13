12101d3d6d26c7e690d87a43cb17e01748a2c8ad
Chandu816 <poojari.chandrahas@gmail.com>
2021-07-13 09:54:27 +0530

    ViewModel Setup Completed DataTemplate INotifyPropertyChanged

--- VS/Normal/SimpleTraderApp/SimpleTraderApp.WPF/Models/ObservableObject.cs
+++ VS/Normal/SimpleTraderApp/SimpleTraderApp.WPF/Models/ObservableObject.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace SimpleTraderApp.WPF.Models
 {
-    public class ObservableObject
+    public class ObservableObject : INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- VS/Normal/SimpleTraderApp/SimpleTraderApp.WPF/State/Navigators/Navigator.cs
+++ VS/Normal/SimpleTraderApp/SimpleTraderApp.WPF/State/Navigators/Navigator.cs
@@ -1,16 +1,27 @@
 ﻿using SimpleTraderApp.WPF.Commands;
+using SimpleTraderApp.WPF.Models;
 using SimpleTraderApp.WPF.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows.Input;
 
 namespace SimpleTraderApp.WPF.State.Navigators
 {
-    public class Navigator : INavigator
+    public class Navigator : ObservableObject,  INavigator 
     {
-        public ViewModelBase CurrentViewModel { get; set; }
+        private ViewModelBase _currentViewModel;
 
         public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
+
+        public ViewModelBase CurrentViewModel { get => _currentViewModel;
+            set 
+            { 
+                _currentViewModel = value;
+                OnPropertyChange(nameof(CurrentViewModel));
+            } 
+        }
+
     }
 }
