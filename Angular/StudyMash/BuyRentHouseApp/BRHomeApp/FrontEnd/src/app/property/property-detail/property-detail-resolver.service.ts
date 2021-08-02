1e2fd9d137b3b202cd5f3b04cdcc4d5a768cb7c7
Chandu816 <poojari.chandrahas@gmail.com>
2021-08-02 09:26:09 +0530

    Added Catch Error for Resolver

--- Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/property/property-detail/property-detail-resolver.service.ts
+++ Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/property/property-detail/property-detail-resolver.service.ts
@@ -1,6 +1,8 @@
+import { Route } from '@angular/compiler/src/core';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Property } from 'src/app/models/Property';
 import { HousingService } from 'src/app/services/housing.service';
 
@@ -9,11 +11,16 @@ import { HousingService } from 'src/app/services/housing.service';
 })
 export class PropertyDetailResolverService implements Resolve<Property> {
 
-  constructor(private housingService : HousingService) { }
+  constructor(private router: Router, private housingService : HousingService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
-     Property | Observable<Property> | Promise<Property> {
+      Observable<any> | Observable<Property> |  Property {
       const id = route.params['propertyid'] ;
-     return this.housingService.getProperty(id);
+     return this.housingService.getProperty(id).pipe(
+       catchError(error => {
+        this.router.navigate(['/']);
+        return of(null);
+       })
+     );
   }
 }
