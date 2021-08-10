a11b61ab589dc29fb97f282f0c83b5c5a1ee44a1
Chandu816 <poojari.chandrahas@gmail.com>
2021-08-10 10:30:17 +0530

    Implementing Post & Delete For City API

--- Angular/StudyMash/BuyRentHouseApp/BRHomeApp/BackEnd/BRHomeWebApi/Controllers/CityController.cs
+++ Angular/StudyMash/BuyRentHouseApp/BRHomeApp/BackEnd/BRHomeWebApi/Controllers/CityController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BRHomeWebApi.DataC;
+using BRHomeWebApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 //using BRHomeWebApi.Models;
 
 namespace BRHomeWebApi.Controllers
@@ -20,11 +22,45 @@ namespace BRHomeWebApi.Controllers
         }
 
         [HttpGet("")]
-        public ActionResult Get()
+        public async Task<ActionResult> Get()
         {
-            var cities = _bRHomeDbContext.Cities.ToList();
+            var cities = await _bRHomeDbContext.Cities.ToListAsync();
             return Ok(cities);
         }
 
+        //api/city/add/{cityName}
+        [HttpPost("add/{cityName}")]
+        public async Task<ActionResult> AddCity(string cityName)
+        {
+            City city = new City()
+            {
+                Name = cityName
+            };
+            await _bRHomeDbContext.Cities.AddAsync(city);
+            await _bRHomeDbContext.SaveChangesAsync();
+            
+            return Ok(city);
+        }
+
+        //api/city/
+        [HttpPost()]
+        public async Task<ActionResult> Post(City city)
+        {
+            await _bRHomeDbContext.Cities.AddAsync(city);
+            await _bRHomeDbContext.SaveChangesAsync();
+            
+            return Ok(city);
+        }
+
+
+        [HttpDelete("delete/{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var city = await _bRHomeDbContext.Cities.FindAsync(id);
+            _bRHomeDbContext.Cities.Remove(city);
+            await _bRHomeDbContext.SaveChangesAsync();
+            
+            return Ok(city);
+        }
     }
 }
