26a8f4188bd942d2303798f0bf4fc6fb3b43c22f
Chandu816 <poojari.chandrahas@gmail.com>
2021-09-23 22:31:51 +0530

    Implement ReTryWhen funtionality

---
+++ Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/enums/ErrorCode.ts
@@ -0,0 +1,7 @@
+export enum ErrorCode{
+  serverDown = 0,
+  UnAuthorized = 401,
+  BadRequest = 400,
+  OkCreated = 201,
+  Success = 200
+}
--- Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/services/httperrors.interceptor.ts
+++ Angular/StudyMash/BuyRentHouseApp/BRHomeApp/FrontEnd/src/app/services/httperrors.interceptor.ts
@@ -6,9 +6,10 @@ import {
   HttpInterceptor,
   HttpErrorResponse
 } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 import { AlertifyService } from './alertify.service';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, concatMap, retryWhen } from 'rxjs/operators';
+import { ErrorCode } from '../enums/ErrorCode';
 
 @Injectable()
 export class HttperrorsInterceptor implements HttpInterceptor {
@@ -17,6 +18,10 @@ export class HttperrorsInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
+      retryWhen(
+        error =>
+          this.retryErrorRequest(error, 10)
+      ),
       catchError((error : HttpErrorResponse) => {
         console.log('Http interceptor detected.')
         console.log(error)
@@ -24,6 +29,21 @@ export class HttperrorsInterceptor implements HttpInterceptor {
         this.alertify.error (errorMessage);
         return throwError(errorMessage);
       })
+    );
+  }
+
+  retryErrorRequest(error : Observable<unknown>, retryCount : number): Observable<unknown>{
+    return error.pipe(
+          concatMap((checkError: unknown, count : number) => {
+            const err = checkError as HttpErrorResponse;
+              if(count <= retryCount){
+                  switch(err.status){
+                    case ErrorCode.serverDown :
+                       return of(err);
+                  }
+              }
+              return throwError(err);
+          })
     );
   }
 
