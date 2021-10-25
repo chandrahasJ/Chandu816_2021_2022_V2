412045a85a2b2cc74d2fca1df97b147b55cd5ce5
Chandu816 <poojari.chandrahas@gmail.com>
2021-10-25 20:32:36 +0530

    Insertion Sort

---
+++ DataStructure/SortAlgos/SortingAlgorithms/SortingAlgorithms/Algorithms/InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms.Algorithms
+{
+    public class InsertionSort :DisplayArray
+    {
+        public InsertionSort()
+        {
+
+        }
+
+        public int[] StartInsertinSorting(int[] unSortedArray, int lengthOfUnSortedArray)
+        {
+            int compareValue = 0;
+            int comparePostion = 0;
+            for (int i = 0; i < lengthOfUnSortedArray; i++)
+            {
+                compareValue = unSortedArray[i];
+                comparePostion = i;
+                while (comparePostion > 0 && unSortedArray[comparePostion -1] > compareValue)
+                {
+                    unSortedArray[comparePostion] = unSortedArray[comparePostion - 1];
+                    comparePostion = comparePostion - 1;
+                }
+                unSortedArray[comparePostion] = compareValue;
+            }
+            return unSortedArray;
+        }
+    }
+}
--- DataStructure/SortAlgos/SortingAlgorithms/SortingAlgorithms/Program.cs
+++ DataStructure/SortAlgos/SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -8,14 +8,20 @@ namespace SortingAlgorithms
         static void Main(string[] args)
         {
             int[] ArrayData = { 5, 3, 6, 8, 2, 1 };
-            SelectionSort selectionSort = new SelectionSort();
+            //SelectionSort selectionSort = new SelectionSort();
 
-            selectionSort.DisplayData(ArrayData, ArrayData.Length, false);
-            selectionSort.StartSelectionSort(ArrayData, ArrayData.Length);
-            selectionSort.DisplayData(ArrayData, ArrayData.Length, false);
+            //selectionSort.DisplayData(ArrayData, ArrayData.Length, false);
+            //selectionSort.StartSelectionSort(ArrayData, ArrayData.Length);
+            //selectionSort.DisplayData(ArrayData, ArrayData.Length, false);
+
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.DisplayData(ArrayData, ArrayData.Length, false);
+            insertionSort.StartInsertinSorting(ArrayData, ArrayData.Length);
+            insertionSort.DisplayData(ArrayData, ArrayData.Length, false);
 
             Console.ReadLine();
 
         }
     }
 }
+
