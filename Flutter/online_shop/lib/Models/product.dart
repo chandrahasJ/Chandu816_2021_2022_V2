afca0c48d8557374afb78e124dcc7b19e602bd77
Chandu816 <poojari.chandrahas@gmail.com>
2021-11-12 22:15:25 +0530

    ListView | ListTitle | List Generate.

--- Flutter/online_shop/lib/Models/product.dart
+++ Flutter/online_shop/lib/Models/product.dart
@@ -1,3 +1,10 @@
+class ProductDetailModel {
+  static final productDetails = [
+    ProductDetail("OS001", "Product 01", "Product Data", 999, "#ffffff",
+        "https://retailminded.com/wp-content/uploads/2016/03/EN_GreenOlive-1.jpg")
+  ];
+}
+
 class ProductDetail {
   final String id;
   final String name;
@@ -9,8 +16,3 @@ class ProductDetail {
   ProductDetail(
       this.id, this.name, this.desc, this.price, this.color, this.image);
 }
-
-final ProductDetails = [
-  ProductDetail("OS001", "Product 01", "Product Data", 999, "#ffffff",
-      "https://retailminded.com/wp-content/uploads/2016/03/EN_GreenOlive-1.jpg")
-];
--- Flutter/online_shop/lib/pages/home_page.dart
+++ Flutter/online_shop/lib/pages/home_page.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:online_shop/Models/product.dart';
 import 'package:online_shop/widgets/custom_drawer.dart';
+import 'package:online_shop/widgets/product_listview.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
   final String appName = "Online Shop";
+
   @override
   Widget build(BuildContext context) {
+    final dummyList =
+        List.generate(20, (index) => ProductDetailModel.productDetails[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(appName),
       ),
-      body: Center(
-        child: Text("Welcome to $appName"),
-      ),
+      body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+              itemCount: dummyList.length,
+              itemBuilder: (context, index) {
+                return ProductListView(productDetail: dummyList[index]);
+              })),
       drawer: const CustomDrawer(),
     );
   }
