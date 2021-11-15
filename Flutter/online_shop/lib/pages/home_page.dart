b6cb4173cf55d6f22a876dacf5daf0a5e5c5aaba
Chandu816 <poojari.chandrahas@gmail.com>
2021-11-15 20:15:29 +0530

    Json via RootBundle, DecodeJson, initState.

--- Flutter/online_shop/lib/pages/home_page.dart
+++ Flutter/online_shop/lib/pages/home_page.dart
@@ -1,11 +1,28 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:online_shop/Models/product.dart';
 import 'package:online_shop/widgets/custom_drawer.dart';
 import 'package:online_shop/widgets/product_listview.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final String appName = "Online Shop";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,5 +41,13 @@ class HomePage extends StatelessWidget {
               })),
       drawer: const CustomDrawer(),
     );
+  }
+
+  void loadData() async {
+    final productJsonData =
+        await rootBundle.loadString("assets/files/products.json");
+    final decodedJson = jsonDecode(productJsonData);
+    var productData = decodedJson["Products"];
+    print(productData);
   }
 }
