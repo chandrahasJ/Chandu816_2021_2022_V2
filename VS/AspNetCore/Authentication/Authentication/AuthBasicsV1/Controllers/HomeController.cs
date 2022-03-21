d5546c42b5ec276a687fd01aad9968bad221d850
chandrahasJ <poojari.chandrahas@gmail.com>
2022-03-21 19:53:03 +0530

    Claims, Claims Identity & Claims Principal

---
+++ VS/AspNetCore/Authentication/Authentication/AuthBasicsV1/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthBasicsV1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        // Authorized Persons only can
+        // access the SecretPage
+        public IActionResult SecretPage()
+        {            
+            return View();
+        }
+
+        //Anyone can get access to NormalPage
+        public IActionResult NormalPage()
+        {
+            return View();
+        }
+
+        public IActionResult Authentication()
+        {
+            // Claims List of key value pair 
+            var driverClaims = new List<Claim>()
+            {
+                new Claim("Name","Chandu"),
+                new Claim("BirthDate", "07-07-1988"),
+                new Claim("License No,","BBHHQWE")
+            };
+
+            // Identity created using the driver claims 
+            var driverIdentity = new ClaimsIdentity(driverClaims, "Driver Identity");
+
+            // Created the user object using driver identity
+            var driver = new ClaimsPrincipal(new[] { driverIdentity });
+
+            // This will create auth cookie
+            HttpContext.SignInAsync(driver);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult UnAuthorize()
+        {
+            return View();
+        }
+    }
+}
--- VS/AspNetCore/Authentication/Authentication/AuthBasicsV1/Startup.cs
+++ VS/AspNetCore/Authentication/Authentication/AuthBasicsV1/Startup.cs
@@ -20,36 +20,35 @@ namespace AuthBasicsV1
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddAuthentication("CookieSchema")
+                    .AddCookie("CookieSchema", config => {
+                        config.Cookie.Name = "Driver.Cookies";
+                        config.LoginPath = "/Home/Authentication";
+                        config.AccessDeniedPath = "/Home/UnAuthorize";
+                    });
+            services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            }            
 
             app.UseRouting();
 
+            // Who are you 
+            app.UseAuthentication();
+
+            // Are you allowed to acces the page 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
