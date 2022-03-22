31cb7e2f0cf0ca347395af6baeef039c009c165c
chandrahasJ <poojari.chandrahas@gmail.com>
2022-03-22 21:32:56 +0530

    Identity Example Day 1

---
+++ VS/AspNetCore/Authentication/Authentication/IdentityExampleV1/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityExampleV1.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> dbContextOptions) : 
+                base(dbContextOptions) 
+        {
+            
+        }
+    }
+}
