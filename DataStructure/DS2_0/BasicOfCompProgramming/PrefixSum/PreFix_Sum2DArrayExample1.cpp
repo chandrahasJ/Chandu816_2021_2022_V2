74e934b98ae155fd962948811b56d2191bc7c0c7
chandrahasJ <poojari.chandrahas@gmail.com>
2022-08-17 21:40:48 +0530

    2d Array Normal - PreFix Sum

---
+++ DataStructure/DS2_0/BasicOfCompProgramming/PrefixSum/PreFix_Sum2DArrayExample1.cpp
@@ -0,0 +1,56 @@
+/*
+    Given 2d Array a of N*N integer.
+    Given Q Queries and in each query given a, b, c and d
+    print the sum of square represented by (a,b) as top left point 
+    and  (c,d) as bottom right point.
+
+    Constraints
+    1 <= N <= 10^3;
+    1 <= a[i][j] <= 10e9;
+    1 <= Q <= 10^5;
+    1 <= a,b,c,d <= N
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+const long long M = 10010;
+const long long N = 10010;
+long long prefix[M][M];
+long long ar[N][N];
+
+void Normal(){
+    int n;
+    cin>>n;
+    
+    for (int i = 1; i <=n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cin>>ar[i][j];
+        }        
+    }
+
+    int Q;
+    cin>>Q;
+    while (Q--)
+    {
+        int a,b,c,d;
+        cin>> a>>b>>c>>d;
+        long long sum = 0;
+        for (int i = a; i <= c; i++)
+        {
+            for (int j = b; j <= d; j++)
+            {
+                sum += ar[i][j];
+            }        
+        }
+        cout<<sum<<endl;
+    }
+    
+    
+}
+
+int main(){ 
+    Normal();
+    return 0;
+}
