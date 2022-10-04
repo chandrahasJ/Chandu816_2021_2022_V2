276e538b0b58683c1b1c080d73a3dfab55e85784
chandrahasJ <poojari.chandrahas@gmail.com>
2022-10-04 22:02:18 +0530

    Added Dummy

--- VS/Normal/Player/YTwitchPlayer/Models/Constants.cs
+++ VS/Normal/Player/YTwitchPlayer/Models/Constants.cs
@@ -12,7 +12,7 @@ namespace YTwitchPlayer.Models
         public static string EmailAddress = "purplepills777p@gmail.com";
         public static string ApplicationId = ".cjsoftwares.ytwitchplayer";
         public static string ApiServiceURL = @"https://youtube.googleapis.com/youtube/v3/";
-        public static string ApiKey = @"";
+        public static string ApiKey = @"AIzaSyAT7UDu2LYMDWHPP09sD7Rq6G-lUSX4CA8";
 
         public static uint MicroDuration { get; set; } = 100;
         public static uint SmallDuration { get; set; } = 300;
