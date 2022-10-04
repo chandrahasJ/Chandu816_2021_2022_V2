7afb5894c2c93aac06fd41cdfa94d102e008193e
chandrahasJ <poojari.chandrahas@gmail.com>
2022-10-04 21:38:26 +0530

    Added Constants

---
+++ VS/Normal/Player/YTwitchPlayer/Models/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTwitchPlayer.Models
+{
+    public static class Constants
+    {
+        public static string ApplicationName = "Twitchy Player";
+        public static string EmailAddress = "purplepills777p@gmail.com";
+        public static string ApplicationId = ".cjsoftwares.ytwitchplayer";
+        public static string ApiServiceURL = @"https://youtube.googleapis.com/youtube/v3/";
+        public static string ApiKey = @"";
+
+        public static uint MicroDuration { get; set; } = 100;
+        public static uint SmallDuration { get; set; } = 300;
+        public static uint MediumDuration { get; set; } = 600;
+        public static uint LongDuration { get; set; } = 1200;
+        public static uint ExtraLongDuration { get; set; } = 1800;
+    }
+}
