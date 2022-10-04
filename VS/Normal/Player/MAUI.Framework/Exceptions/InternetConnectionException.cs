ecea5331658f75cebc206486faaadb5bbce5ae7c
chandrahasJ <poojari.chandrahas@gmail.com>
2022-10-04 17:37:29 +0530

    Added Custom Exception -  InternetConnectionException

---
+++ VS/Normal/Player/MAUI.Framework/Exceptions/InternetConnectionException.cs
@@ -0,0 +1,17 @@
+﻿namespace MAUI.Framework.Exceptions
+{
+    public class InternetConnectionException : Exception
+    {
+        public InternetConnectionException()
+        {
+        }
+
+        public InternetConnectionException(string message) : base(message)
+        {
+        }
+
+        public InternetConnectionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
