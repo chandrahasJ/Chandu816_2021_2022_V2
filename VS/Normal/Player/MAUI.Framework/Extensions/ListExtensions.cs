c13de79968df45b0e08b8ea0cabe302906a384d5
chandrahasJ <poojari.chandrahas@gmail.com>
2022-10-04 20:03:32 +0530

    Added List Extensions

---
+++ VS/Normal/Player/MAUI.Framework/Extensions/ListExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI.Framework.Extensions
+{
+    public static class ListExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (var item in enumeration)
+            {
+                action(item);
+            }
+        }
+
+        public static IEnumerable<T> Randomize<T>(this IEnumerable<T> source)
+        {
+            Random random = new();
+            return source.OrderBy<T, int>(item => random.Next());
+        }
+
+        public static void AddRange<T>(this ObservableCollection<T> collection, 
+                                            IEnumerable<T> newItems, 
+                                            bool clearFirst = false)
+        {
+            if (clearFirst)
+            {
+                collection.Clear();
+            }
+
+            newItems.ForEach(newItem => collection.Add(newItem));
+        }
+    }
+}
--- VS/Normal/Player/MAUI.Framework/GlobalUsings.cs
+++ VS/Normal/Player/MAUI.Framework/GlobalUsings.cs
@@ -4,3 +4,4 @@ global using MAUI.Framework.Extensions;
 global using System.Net.Http.Headers;
 global using System.Text.Json;
 global using System.Text;
+global using System.Collections.ObjectModel;
