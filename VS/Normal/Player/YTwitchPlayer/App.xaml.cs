bd5f194ebc4466cebe55f9d8de0188659b068304
chandrahasJ <poojari.chandrahas@gmail.com>
2022-10-04 17:25:16 +0530

    Set Up App Size on MS Windows

--- VS/Normal/Player/YTwitchPlayer/App.xaml.cs
+++ VS/Normal/Player/YTwitchPlayer/App.xaml.cs
@@ -1,13 +1,34 @@
-﻿namespace YTwitchPlayer;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+namespace YTwitchPlayer;
 
 public partial class App : Application
 {
+	const int WindowWidth = 540;
+	const int WindowHeight = 800;
 	public App()
 	{
 		InitializeComponent();
 
 		//Enable Version Tracking
 		VersionTracking.Track();
+
+		//Set Up App Size on MS Windows
+		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+		{
+#if WINDOWS
+			var mauiWindow = handler.VirtualView;
+			var nativeWindow = handler.PlatformView;
+			nativeWindow.Activate();
+			IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			WindowId windowId= Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+			AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+			appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+#endif
+        });
 
 		MainPage = new AppShell();
 	}
