943dfc9acebbd01ca1cc6273cf3f1afa9d70c604
chandrahasJ <poojari.chandrahas@gmail.com>
2022-10-24 10:09:29 +0530

    Added Numeric Extensions - for custom view counts

---
+++ VS/Normal/Player/MAUI.Framework/Extensions/NumericExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUIApp.Framework.Extensions
+{
+    public static class NumericExtensions
+    {
+        public static string FormattedNumber(this double number) =>
+            number switch
+	        {
+                >= 1000000 => (number / 1000000D).ToString("0.#M"),
+                >= 10000   => (number / 10000).ToString("0.#K"),
+                _ => number.ToString("#,0")
+	        };
+    }
+}
