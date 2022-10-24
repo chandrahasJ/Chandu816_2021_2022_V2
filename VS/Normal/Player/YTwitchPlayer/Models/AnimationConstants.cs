bef0bdee1c06b1ee5dd357d6a57782dc9d8dbc10
chandrahasJ <poojari.chandrahas@gmail.com>
2022-10-24 14:28:03 +0530

    Implementing Animations Logic

---
+++ VS/Normal/Player/YTwitchPlayer/Models/AnimationConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTwitchPlayer.Models
+{
+    public class AnimationConstants
+    {
+        public const string TransitionAnimation = "TransitionAnimation";
+    }
+}
--- VS/Normal/Player/YTwitchPlayer/Views/VideoDetailsPage.xaml.cs
+++ VS/Normal/Player/YTwitchPlayer/Views/VideoDetailsPage.xaml.cs
@@ -5,5 +5,42 @@ public partial class VideoDetailsPage : ViewBase<VideoDetailsPageViewModel>
 	public VideoDetailsPage(object initParams) : base(initParams)
 	{
 		InitializeComponent();
+
+		this.ViewModelInitialized += (s , e) =>
+		{
+			(this.BindingContext as VideoDetailsPageViewModel).DownloadCompleted += VideoDetailsPage_DownloadCompleted;
+		};
 	}
+
+	protected override void OnDisappearing()
+	{
+        (this.BindingContext as VideoDetailsPageViewModel).DownloadCompleted -= VideoDetailsPage_DownloadCompleted;
+        base.OnDisappearing();
+	}
+
+	private void VideoDetailsPage_DownloadCompleted(object sender, EventArgs e)
+	{
+		// Video information
+		if ((this.BindingContext as VideoDetailsPageViewModel).IsErrorState)
+		{
+			return;
+		}
+
+		if (this.AnimationIsRunning(AnimationConstants.TransitionAnimation))
+		{
+			return;
+		}
+
+		var parentAnimation = new Animation();
+
+		//Poster Image Animation
+		parentAnimation.Add(0.0, 0.7, new Animation(v => HeaderView.Opacity = v, 0, 1, Easing.CubicIn));
+
+		//Video Title 
+        parentAnimation.Add(0.0, 0.7, new Animation(v => VideoTitle.Opacity = v, 0, 1, Easing.CubicIn));
+
+		parentAnimation.Commit(this, AnimationConstants.TransitionAnimation, 16, Constants.ExtraLongDuration, null, (v, c) => { 
+			// Perform some activity after animation is completed.
+		});
+    }
 }
