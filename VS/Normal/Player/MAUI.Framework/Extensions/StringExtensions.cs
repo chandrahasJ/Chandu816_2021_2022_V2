5e8c9198f115f64e8dc94298ff4dbb535930b692
chandrahasJ <poojari.chandrahas@gmail.com>
2022-10-24 10:15:19 +0530

    Added extra functionality for String Extensions - FormattedNumber

--- VS/Normal/Player/MAUI.Framework/Extensions/StringExtensions.cs
+++ VS/Normal/Player/MAUI.Framework/Extensions/StringExtensions.cs
@@ -6,5 +6,8 @@ namespace MAUIApp.Framework.Extensions
     {
         public static string CleanCacheKey(this string uri) =>
             Regex.Replace(new Regex("[\\~#%&*{}/:<>?|\"-]").Replace(uri, " "), @"\s+", "-");
+
+        public static string FormattedNumber(this string number) =>
+            Convert.ToDouble(number).FormattedNumber();
     }
 }
