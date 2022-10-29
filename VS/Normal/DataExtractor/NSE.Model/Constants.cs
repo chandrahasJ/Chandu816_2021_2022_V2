7383645ebbfdf0eda28099fe1be04dd6ce31f4a4
chandrahasJ <poojari.chandrahas@gmail.com>
2022-10-29 23:39:39 +0530

    Added New Constants

--- VS/Normal/DataExtractor/NSE.Model/Constants.cs
+++ VS/Normal/DataExtractor/NSE.Model/Constants.cs
@@ -10,5 +10,17 @@ namespace NSE.Model
     {
         public static string ApiServiceURL = "https://www.nseindia.com/api/";
         public static string OptionChainQuery = "option-chain-indices?symbol={0}";
+        public static string SettingFileName = $"NSE_Settings.json";
+        public static string JsonFolderName = "SettingFiles";
+        public static string JsonFolderPath = AppDomain.CurrentDomain.BaseDirectory + "\\" + JsonFolderName + "\\";
+        public static string ExcelFolderName = "ExcelFiles";
+        public static string ExcelFolderPath = AppDomain.CurrentDomain.BaseDirectory + "\\" + ExcelFolderName + "\\";
+        public static List<string> Symbols = new List<string>()
+        {
+            "NIFTY",
+            "FINNIFTY",
+            "BANKNIFTY",
+            "MIDCPBIFTY"
+        };
     }
 }
