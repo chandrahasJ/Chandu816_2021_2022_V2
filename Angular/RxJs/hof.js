7d4236f78379a420dde82af843b8ff90e8f049b6
chandrahasJ <poojari.chandrahas@gmail.com>
2023-03-26 12:50:19 +0530

    HOF Observable

--- Angular/RxJs/hof.js
+++ Angular/RxJs/hof.js
@@ -16,3 +16,28 @@ function logMe(msg){
 // return value
 hof("CJ", myFunction);
 // Output : My name is CJ.
+
+
+// Simple HOF Observable -  
+function ObservableHOF(observer){
+    let counter = 1;
+    // Producer the emits data every 1 sec   
+    const producer = setInterval(() => {
+        observer.next(counter++);
+    }, 1000);
+
+    // Subscription - Cancel / Terminate the Observable
+    return () => {
+        clearInterval(producer)
+    };
+}
+
+const subscription = ObservableHOF({
+    next : (data) => console.log(data),
+    error : (error) => console.log('error '+ error),
+    completed : () => console.log('Done'),
+});
+
+setInterval(() => {
+    subscription();
+}, 5000)
