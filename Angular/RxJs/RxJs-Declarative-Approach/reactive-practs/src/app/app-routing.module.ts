67990609337ddd7eacaa9f1a7db34b9628fd5b35
chandrahasJ <poojari.chandrahas@gmail.com>
2023-04-09 18:32:15 +0530

    Initial Setup 2

--- Angular/RxJs/RxJs-Declarative-Approach/reactive-practs/src/app/app-routing.module.ts
+++ Angular/RxJs/RxJs-Declarative-Approach/reactive-practs/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './pages/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PostsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
