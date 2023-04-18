f38bea157ac0a6bfef8bb44e9f8631be770dd405
chandrahasJ <poojari.chandrahas@gmail.com>
2023-04-18 21:40:41 +0530

    Adding dropdown list - change event not working

--- Angular/RxJs/RxJs-Declarative-Approach/reactive-practs/src/app/pages/declarative-post/declarative-post.component.ts
+++ Angular/RxJs/RxJs-Declarative-Approach/reactive-practs/src/app/pages/declarative-post/declarative-post.component.ts
@@ -1,15 +1,38 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { map } from 'rxjs';
+import { DeclarativeCategoryService } from 'src/app/services/declarative-category.service';
 import { DeclarativePostService } from 'src/app/services/declarative-post.service';
 
 @Component({
   selector: 'app-declarative-post',
   templateUrl: './declarative-post.component.html',
   styleUrls: ['./declarative-post.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DeclarativePostComponent {
-  dPost$ = this.dPostService.post_with_category;
+  selectedCategoryId = '';
 
-  constructor(private dPostService: DeclarativePostService) {
+  dPost$ = this.dPostService.post_with_category;
+  dCategory$ = this.dCategoryService.category_data;
+
+  dFilterData$ = this.dPost$.pipe(
+    map((posts) => {
+      return posts.filter((post) =>
+        this.selectedCategoryId
+          ? post.categoryId === this.selectedCategoryId
+          : true
+      );
+    })
+  );
+
+  onCategoryChange(event: Event) {
+    let selectedCategoryId = (event.target as HTMLSelectElement).value;
+    this.selectedCategoryId = selectedCategoryId;
+    console.log(this.selectedCategoryId);
   }
+
+  constructor(
+    private dPostService: DeclarativePostService,
+    private dCategoryService: DeclarativeCategoryService
+  ) {}
 }
