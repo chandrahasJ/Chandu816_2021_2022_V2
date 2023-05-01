7f72ccb041e7d213eedb1ad29d4053b0766c114b
chandrahasJ <poojari.chandrahas@gmail.com>
2023-05-01 18:51:10 +0530

    Select "Active" catergory

--- Angular/RxJs/RxJs-Declarative-Approach/reactive-practs/src/app/pages/alt-posts/alt-posts.component.ts
+++ Angular/RxJs/RxJs-Declarative-Approach/reactive-practs/src/app/pages/alt-posts/alt-posts.component.ts
@@ -1,14 +1,21 @@
-import { Component, Inject } from '@angular/core';
-import { map } from 'rxjs';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
 import { IPost } from 'src/app/models/Post';
 import { DeclarativePostService } from 'src/app/services/declarative-post.service';
 
 @Component({
   selector: 'app-alt-posts',
   templateUrl: './alt-posts.component.html',
-  styleUrls: ['./alt-posts.component.scss']
+  styleUrls: ['./alt-posts.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AltPostsComponent {
+  // selectedPostIdSubject = new BehaviorSubject<string>('');
+  // selectedPostIdAction$ = this.postIdSubject.asObservable();
+  // [class] = "{active: (selectedPostIdAction$ | async) === post.id}"
+
+  selectedPost$ = this.postService.post;
+
   post$ = this.postService?.post_with_category.pipe(map((posts) => {
     return posts.filter(post => post.categoryName !== undefined)
   }));
@@ -18,6 +25,7 @@ export class AltPostsComponent {
 
   onSelectedPost(post: IPost, event: Event){
     event.preventDefault();
-    post.id && this.postService.selectPost(post.id)
+    post.id && this.postService.selectPost(post.id);
+    //post.id && this.postIdSubject.next(post.id);
   }
 }
